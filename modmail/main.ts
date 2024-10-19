686e356f8682d6e14e56afaef9273d3dd6eb9bfd
Zack-szh <48872610+Zack-szh@users.noreply.github.com>
2024-10-19 16:45:46 -0400

    demo done

--- modmail/main.ts
+++ modmail/main.ts
@@ -1,34 +1,34 @@
 import getModmail, { fetchBannedUsers } from './getModmail';
 import * as Modmail_interface from './modmail_interface';
 
-interface BannedUser { 
+interface User { 
     name: string; 
     conversationId: string;
 }
 
-const bannedUsers: BannedUser[] = []; 
+const bannedUsers: User[] = []; 
 const banned_list: string[] = []; 
 
 async function main() { 
     console.log("hello world!");
     try { 
-        const data = await getModmail(); 
-        //console.log(data); 
-        //processResponse(data); 
-        
-        fetchBannedUsers('appealmodtest', 50).then(bannedUsers => {
-            //console.log('Banned Users:', bannedUsers);
-            //console.log('Banned Users:', bannedUsers.children[0].name);
+        // First, fetch the banned users
+        const bannedUsersData = await fetchBannedUsers('appealmodtest', 50);
+        for (let i = 0; i < bannedUsersData.children.length; i++) {
+            banned_list.push(bannedUsersData.children[i].name);
+        }
 
-            for (let i = 0; i < bannedUsers.children.length; i++){ 
-                //console.log('Banned Users:', bannedUsers.children[i].name); 
-                banned_list.push(bannedUsers.children[i].name); 
-            }
-            console.log(banned_list); 
-        });
+        // Then, fetch and process the modmail data
+        const data = await getModmail();
+        processResponse(data);
 
-    } catch (error) { 
-        console.error('Error fetching modmail:', error);
+        // Now print the banned users
+        for (let i = 0; i < bannedUsers.length; i++) {
+            console.log(`Banned User: ${bannedUsers[i].name} - Conversation ID: ${bannedUsers[i].conversationId}`);
+        }
+
+    } catch (error) {
+        console.error('Error in main function:', error);
     }
 }
 
@@ -51,17 +51,19 @@ function processResponse(response: Modmail_interface.Modmail) {
                 console.log(`Author: ${message.author.name}`);
                 console.log(`Date: ${message.date}`);
                 console.log(`Body: ${message.bodyMarkdown}`);
-                isBanned(message.author); 
+                if (isBanned(message.author.name)){ 
+                    bannedUsers.push({name: message.author.name, conversationId: convId}); 
+                }
             }
         }
         console.log(`--------------------------------`);
     }
 }
 
-async function isBanned(user: Modmail_interface.ParticipantElement) : Promise<boolean> { 
+function isBanned(name: string) : boolean { 
     //we already have a list of banned users in banned_list
     //we can check if the user is in the list and return true if they are, false otherwise
-    return banned_list.includes(user.name); 
+    return banned_list.includes(name); 
 }
 
 
